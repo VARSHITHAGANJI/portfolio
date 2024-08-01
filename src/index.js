54f24b4d667fad5bf16d60fba681eeea213498e6
Varshitha <ganjivarshitha9@gmail.com>
2024-08-01 22:35:11 +0530

    fix: hash routing added

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
+// index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
-import './styles/index.css';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <HashRouter>
-      <App />
-    </HashRouter>
-  </React.StrictMode>
+ 
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+   <React.StrictMode>
+      <HashRouter>
+         <App />
+      </HashRouter>
+   </React.StrictMode>
 );
