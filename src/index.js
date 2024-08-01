633f7686ec7b20efa66dae6b4c4afb969f5c1cfb
Varshitha <ganjivarshitha9@gmail.com>
2024-08-01 22:32:52 +0530

    fix: hash route added for github

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-// index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import App from './App';
- 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-   <React.StrictMode>
-      <BrowserRouter>
-         <App />
-      </BrowserRouter>
-   </React.StrictMode>
+import './styles/index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
