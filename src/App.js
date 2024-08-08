48d7900fa5013ec7535b6943497cd0e63179ee96
Varshitha <ganjivarshitha9@gmail.com>
2024-08-08 06:59:04 +0530

    build: made changes for navbar and toggle

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-// App.js
+import {useState} from "react"
 import { Routes, Route } from 'react-router-dom';
-import NavBar from "./Components/Navbar";
+import NavBar from "./Components/Navbar/Navbar";
 import Home from './Pages/Home';
 import Experience from './Pages/Experience';
-// import Products from './Pages/Products';
+
+import { useContext } from "react";
+
+
+import './App.css';
+
  
 const App = () => {
+  const [isDarkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = (checked: boolean) => {
+    setDarkMode(checked);
+  };
    return (
-      <>
+      <div>
+      
       <NavBar />
          <Routes>
             <Route path="/" element={<Home />} />
-            {/* <Route path="/products" element={<Products />} /> */}
             <Route path="/experience" element={<Experience />} />
          </Routes>
-      </>
+      </div>
    );
 };
  
--- src/index.js
+++ src/index.js
@@ -1,15 +1,18 @@
-// index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import "./index.css"
 import { HashRouter } from 'react-router-dom';
+
 import App from './App';
  
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
    <React.StrictMode>
       <HashRouter>
+
          <App />
+
       </HashRouter>
    </React.StrictMode>
 );
